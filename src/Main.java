796acef9333968ddb775290b811e9edcca8c9d9d
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-04 13:25:43 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,91 @@
+class Weapon {
+    private String type;
+    private String name;
+
+    public Weapon(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
+class GameEntity {
+    private String name;
+    private int health;
+    private int damage;
+
+    public GameEntity(String name, int health, int damage) {
+        this.name = name;
+        this.health = health;
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+}
+
+class Boss extends GameEntity {
+    private Weapon weapon;
+
+    public Boss(String name, int health, int damage, Weapon weapon) {
+        super(name, health, damage);
+        this.weapon = weapon;
+    }
+
+    public Weapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(Weapon weapon) {
+        this.weapon = weapon;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Weapon bossWeapon = new Weapon("Меч", "Огненный клинок");
+
+        Boss boss = new Boss("Могучий Босс", 1000, 50, bossWeapon);
+
+        System.out.println("Имя босса: " + boss.getName());
+        System.out.println("Здоровье босса: " + boss.getHealth());
+        System.out.println("Урон босса: " + boss.getDamage());
+        System.out.println("Оружие босса: " + boss.getWeapon().getName() + " (" + boss.getWeapon().getType() + ")");
     }
 }
